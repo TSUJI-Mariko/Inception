ba036b39389a8e0aafd8dfb0466509200c89bbe5
Mariko TSUJI <mtsuji@e3r1p1.clusters.42paris.fr>
2023-05-05 19:29:58 +0200

    rooooooooot

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -17,8 +17,10 @@ n
 Y
 Y
 _EOF_
+
 echo "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-echo "UPDATE mysql.user SET host = 'localhost' WHERE user = 'root' AND host = '%'; FLUSH PRIVILEGES;" | mysql -u root
+echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User='root' AND plugin='unix_socket'; FLUSH PRIVILEGES;" | mysql -u root
+#echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
 fi
